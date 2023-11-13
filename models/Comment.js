2c367af3bda55635192da8a4920c23521d493b23
A K <akahly@gmail.com>
2023-11-13 12:04:02 -0500

    First models entered

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+class Comment extends Model {}
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [3],
+      },
+    },
+    userid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    postid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+module.exports = Comment;
