14d67406dea50326c7659d37b6f393333d7efe4f
A K <akahly@gmail.com>
2023-11-27 19:16:12 -0500

    seeds added

---
+++ seeds/user-seeds.js
@@ -0,0 +1,32 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    username: "MarkLarrs",
+    password: "password567",
+  },
+  {
+    username: "Zyndi",
+    password: "pass1word2",
+  },
+  {
+    username: "Slixx",
+    password: "password3333",
+  },
+  {
+    username: "Jemii",
+    password: "password444",
+  },
+  {
+    username: "Parzo",
+    password: "password5555",
+  },
+  {
+    username: "Jimjam",
+    password: "password6666",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
